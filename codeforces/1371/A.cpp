2ca3ae25a4599d4208644158f4b28debd75b6a19
Sifat Hossain <sifatb910@gmail.com>
2020-07-01 20:43:00 +0600

    Add solution for problem `A - Magical Sticks` Link: https://codeforces.com/contest/1371/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1371/submission/85670137

---
+++ codeforces/1371/A.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+#define ll long long
+#define sci(x)          scanf("%d",&x)
+#define scii(x,y)       scanf("%d%d",&x,&y)
+#define sciii(x,y,z)    scanf("%d%d%d",&x,&y,&z)
+#define scl(x)          scanf("%ld",&x)
+#define scll(x,y)       scanf("%ld%ld",&x,&y)
+#define sclll(x,y,z)    scanf("%ld%ld%ld",&x,&y,&z)
+#define scL(x)          scanf("%lld",&x)
+#define scLL(x,y)       scanf("%lld%lld",&x,&y)
+#define scLLL(x,y,z)    scanf("%lld%lld%lld",&x,&y,&z)
+#define scs(s)          scanf("%s",s)
+
+#define TC(t)           int t;sc("%d",&t);while(t--)
+#define FTC(t)          int t;sc("%d",&t);for(int tt=1;tt<=t;tt++)
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+int main(){
+
+    int t;
+    cin >> t ;
+
+    while (t--){
+
+        ll n;
+        cin >> n;
+
+        if (n%2){
+            cout << n/2 +1 << endl;
+        }
+        else {
+            cout << n/2 << endl;
+        }
+    }
+    return 0;
+}
+
