9198d89135514ae576732228866bce07351ad21f
Sifat Hossain <sifatb910@gmail.com>
2020-09-14 21:06:00 +0600

    Add solution for problem `A - Buying Torches` Link: https://codeforces.com/contest/1418/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/1418/submission/92811146

---
+++ codeforces/1418/A.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+#define ll long long
+#define sci(x)          scanf("%d",&x)
+#define scii(x,y)       scanf("%d%d",&x,&y)
+#define sciii(x,y,z)    scanf("%d%d%d",&x,&y,&z)
+#define scl(x)          scanf("%ld",&x)
+#define scll(x,y)       scanf("%ld%ld",&x,&y)
+#define sclll(x,y,z)    scanf("%ld%ld%ld",&x,&y,&z)
+#define scL(x)          scanf("%lld",&x)
+#define scLL(x,y)       scanf("%lld%lld",&x,&y)
+#define scLLL(x,y,z)    scanf("%lld%lld%lld",&x,&y,&z)
+#define scs(s)          scanf("%s",s)
+
+#define TC(t)           int t;sc("%d",&t);while(t--)
+#define FTC(t)          int t;sc("%d",&t);for(int tt=1;tt<=t;tt++)
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        ll x,y,k;
+        cin >> x >> y >> k;
+
+        ll cnt = k;
+
+        ll c ;
+
+        if ( (k+ y*k -1)% (x-1)== 0 ){
+            c = (k+ y*k -1)/(x-1);
+        }
+        else {
+            c =(k+ y*k -1)/(x-1) +1 ;
+        }
+        cnt += c;
+
+        cout << cnt  << endl;
+
+
+    }
+}
+
