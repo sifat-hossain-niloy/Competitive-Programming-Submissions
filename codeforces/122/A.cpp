d4fbc6f3ab6e63973c769c924c42ed158680d30e
Sifat Hossain <sifatb910@gmail.com>
2020-10-09 22:27:00 +0600

    Add solution for problem `A - Lucky Division` Link: https://codeforces.com/contest/122/problem/A Tags: brute force, number theory, *1000 Ref: https://codeforces.com/contest/122/submission/95004774

---
+++ codeforces/122/A.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+#define ll long long
+#define sci(x)          scanf("%d",&x)
+#define scii(x,y)       scanf("%d%d",&x,&y)
+#define sciii(x,y,z)    scanf("%d%d%d",&x,&y,&z)
+#define scl(x)          scanf("%ld",&x)
+#define scll(x,y)       scanf("%ld%ld",&x,&y)
+#define sclll(x,y,z)    scanf("%ld%ld%ld",&x,&y,&z)
+#define scL(x)          scanf("%lld",&x)
+#define scLL(x,y)       scanf("%lld%lld",&x,&y)
+#define scLLL(x,y,z)    scanf("%lld%lld%lld",&x,&y,&z)
+#define scs(s)          scanf("%s",s)
+
+#define TC(t)           int t;sc("%d",&t);while(t--)
+#define FTC(t)          int t;sc("%d",&t);for(int tt=1;tt<=t;tt++)
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    if ( t%4 && t%7 && t%47 && t%74 && t%447 && t%474 && t%444 && t%477 && t%777 && t%744 && t%747) cout << "NO" ;
+    else cout << "YES" ;
+    return 0;
+}
