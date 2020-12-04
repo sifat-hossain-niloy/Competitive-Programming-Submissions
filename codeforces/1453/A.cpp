378c3ff55cfdd8a21e7d9f884e07c19891ecae2d
Sifat Hossain <sifatb910@gmail.com>
2020-12-04 23:19:00 +0600

    Add solution for problem `A - Cancel the Trains` Link: https://codeforces.com/contest/1453/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1453/submission/100394284

---
+++ codeforces/1453/A.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+#include<stdio.h>
+
+using namespace std;
+
+#define ll long long
+#define sci(x)          scanf("%d",&x)
+#define scii(x,y)       scanf("%d%d",&x,&y)
+#define sciii(x,y,z)    scanf("%d%d%d",&x,&y,&z)
+#define scl(x)          scanf("%ld",&x)
+#define scll(x,y)       scanf("%ld%ld",&x,&y)
+#define sclll(x,y,z)    scanf("%ld%ld%ld",&x,&y,&z)
+#define scL(x)          scanf("%lld",&x)
+#define scLL(x,y)       scanf("%lld%lld",&x,&y)
+#define scLLL(x,y,z)    scanf("%lld%lld%lld",&x,&y,&z)
+#define scs(s)          scanf("%s",s)
+
+#define TC(x)           int x;sc("%d",&x);while(x--)
+#define FTC(t)          int t;sc("%d",&t);for(int tt=1;tt<=t;tt++)
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n,m, ans = 0 ;
+        cin >> n>> m ;
+
+        int a[102] = {0};
+
+        for ( int i = 0 ; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            a[x]++;
+        }
+        for ( int i = 0 ; i< m ; i++)
+        {
+            int y;
+            cin >> y;
+            if ( a[y]== 1)
+            {
+                ans ++ ;
+
+            }
+        }
+        cout << ans << endl;
+
+    }
+
+    return 0;
+}
