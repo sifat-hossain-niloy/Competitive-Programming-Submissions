635c2a40ebf727b7d5f079e8179c913ddf324dd7
Sifat Hossain <sifatb910@gmail.com>
2021-03-12 23:50:00 +0600

    Add solution for problem `A - Elephant` Link: https://codeforces.com/contest/617/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/617/submission/109805656

---
+++ codeforces/617/A.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+#define ll long long
+
+#define TC(x)           int x;sc("%d",&x);while(x--)
+#define FL(t)           for(int i=0;i<t;i++)
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+int main(){
+
+    int x;
+    cin >> x;
+
+    int ans =  x/5;
+
+    if (x%5==0) cout << ans;
+    else cout << ans+1;
+
+    return 0;
+}
+
