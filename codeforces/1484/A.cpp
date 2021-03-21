231b9282e5d7bcb5b09d3bdf76912d7c727b1343
Sifat Hossain <sifatb910@gmail.com>
2021-03-21 20:11:00 +0600

    Add solution for problem `A - Prison Break` Link: https://codeforces.com/contest/1484/problem/A Tags: constructive algorithms, *800 Ref: https://codeforces.com/contest/1484/submission/110645105

---
+++ codeforces/1484/A.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+    int n,m;
+    cin >> n >> m;
+
+    cout << m*n << endl;
+
+    }
+    
+    return  0;
+}
