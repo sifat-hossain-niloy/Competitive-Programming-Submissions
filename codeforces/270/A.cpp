88c35261f6aebd5175f1516f16abf10a1b640f16
Sifat Hossain <sifatb910@gmail.com>
2021-04-19 02:47:00 +0600

    Add solution for problem `A - Fancy Fence` Link: https://codeforces.com/contest/270/problem/A Tags: geometry, implementation, math, *1100 Ref: https://codeforces.com/contest/270/submission/113442437

---
+++ codeforces/270/A.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+#define ll long long
+
+#define TC(x)           int x;sc("%d",&x);while(x--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+int main(){
+    
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        n = 180-n;
+
+        if (360%n) cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+
+    return 0;
+}
