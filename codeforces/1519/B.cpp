347a7bb89f262828a4fe47fbd8ad94766b8da5f0
Sifat Hossain <sifatb910@gmail.com>
2021-04-29 20:53:00 +0600

    Add solution for problem `B - The Cake Is a Lie` Link: https://codeforces.com/contest/1519/problem/B Tags: dp, math, *800 Ref: https://codeforces.com/contest/1519/submission/114558835

---
+++ codeforces/1519/B.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<cstring>
+#include<vector>
+#include<set>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main(){
+    
+    TC(t)
+    {
+        int n,m,k;
+        cin >> n >> m >>k;
+
+        if (k==(n*m-1))
+        {
+            cout << "YES\n";
+        }
+        else {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
