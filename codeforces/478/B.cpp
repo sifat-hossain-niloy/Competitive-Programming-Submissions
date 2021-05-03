1be8e5d91288c74089e41c56013c983c380fb20a
Sifat Hossain <sifatb910@gmail.com>
2021-05-03 22:10:00 +0600

    Add solution for problem `B - Random Teams` Link: https://codeforces.com/contest/478/problem/B Tags: combinatorics, constructive algorithms, greedy, math, *1300 Ref: https://codeforces.com/contest/478/submission/115053094

---
+++ codeforces/478/B.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<cstring>
+#include<vector>
+#include<set>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+long long C(int n, int r) {
+    if (r>n) return 0 ;
+    if(r > n - r) r = n - r; // because C(n, r) == C(n, n - r)
+    long long ans = 1;
+    int i;
+
+    for(i = 1; i <= r; i++) {
+        ans *= n - r + i;
+        ans /= i;
+    }
+
+    return ans;
+}
+ll combi(ll n,ll k)
+{
+    ll ans=1;
+    k=k>n-k?n-k:k;
+    int j=1;
+    for(;j<=k;j++,n--)
+    {
+        if(n%j==0)
+        {
+            ans*=n/j;
+        }else
+        if(ans%j==0)
+        {
+            ans=ans/j*n;
+        }else
+        {
+            ans=(ans*n)/j;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    
+    ll n,m;
+    cin >> n >> m;
+    
+    ll x = n/m;
+    ll y = n%m;
+
+    ll kmin  = y*C(x+1,2)+(m-y)*(C(x,2));
+    cout << kmin << " ";
+
+    ll kmax = C(n-m+1,2);
+    cout << kmax;
+
+    return 0;
+}
