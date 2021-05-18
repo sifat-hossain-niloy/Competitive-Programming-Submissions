57740ad43b8df902b43b6a85f6d839d468f20af8
Sifat Hossain <sifatb910@gmail.com>
2021-05-18 16:52:00 +0600

    Add solution for problem `B - Fence` Link: https://codeforces.com/contest/363/problem/B Tags: brute force, dp, *1100 Ref: https://codeforces.com/contest/363/submission/116556020

---
+++ codeforces/363/B.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<cstring>
+#include<vector>
+#include<set>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main(){
+    
+    
+    int n,k;
+    cin >> n >>k;
+    ll a[n+1],b[n+1];
+    b[0] = 0;
+
+    for (int i = 1; i<=n; i++)
+    {
+        cin >> a[i];
+        b[i] = b[i-1]+a[i];
+    }
+
+    ll mi = INT32_MAX,index = 0;
+
+    for (int i = 1;i<=n-k+1;i++)
+    {
+        a[i] = b[i+k-1] - b[i-1];
+        mi = min(mi,a[i]);
+    }
+    for (int i = 1; i<=n-k+1 ; i++)
+    {
+        if (a[i]==mi)
+        {
+            index = i;
+            break;
+        }
+    }
+    cout << index  << endl;
+
+    return 0;
+}
