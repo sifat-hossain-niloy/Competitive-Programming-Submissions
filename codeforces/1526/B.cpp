9e4c6bba6743e01cd6bba096a60757f26b549deb
Sifat Hossain <sifatb910@gmail.com>
2021-05-28 22:11:00 +0600

    Add solution for problem `B - I Hate 1111` Link: https://codeforces.com/contest/1526/problem/B Tags: dp, math, number theory, *1400 Ref: https://codeforces.com/contest/1526/submission/117672984

---
+++ codeforces/1526/B.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<cstring>
+#include<vector>
+#include<set>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main(){
+    
+    
+    TC(t)
+    {
+        
+        ll n;
+        cin >> n;
+
+        int y = (n -(n%11)*111);
+        
+
+        if (y>=0 && y%11==0)
+        {
+            Y;
+        }
+        else 
+        {
+            N;
+        }
+
+        
+
+    }
+
+    return 0;
+}
