be2375ab3b71c82d1172de3ce8502c17f3377f79
Sifat Hossain <sifatb910@gmail.com>
2021-06-18 20:45:00 +0600

    Add solution for problem `A - Arithmetic Array` Link: https://codeforces.com/contest/1537/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1537/submission/119815231

---
+++ codeforces/1537/A.cpp
@@ -0,0 +1,64 @@
+    #include<bits/stdc++.h>
+    #include<cmath>
+    #include<cstring>
+    #include<vector>
+    #include<set>
+     
+    using namespace std;
+     
+    #define MOD 1000000007
+    #define ll long long
+    #define TC(t)           int t;cin >> t;while(t--)
+    #define FL(t)           for(int i=0;i<t;i++)
+    #define Y               cout << "YES\n"
+    #define N               cout << "NO\n"
+     
+    #define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    #define read            freopen("in.txt","r",stdin)
+    #define write           freopen("out.txt","w",stdout)
+     
+    long long binpow(long long a, long long b) {
+        long long res = 1;
+        while (b > 0) {
+            if (b & 1)
+                res = res * a % MOD;
+            a = a * a;
+            b >>= 1;
+        }
+        return res;
+    }
+     
+    int main(){
+        
+        TC(t)
+        {
+            int n;
+            cin >> n;
+            ll sum = 0;
+
+            FL(n)
+            {
+                int x;
+                cin >> x;
+
+                sum+=x;
+
+            }
+            if(sum==n)
+            {
+                cout << 0 << endl;
+            }
+            else if (sum<n)
+            {
+                cout << 1 << endl;
+            }
+            else 
+            {
+                cout << abs(n-sum) << endl;
+            }
+            
+            
+        }
+     
+        return 0;
+    }
