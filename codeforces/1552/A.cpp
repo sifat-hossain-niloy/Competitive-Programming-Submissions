01d1498952750ac1526a6cfaa4f4e93cfcd7d78d
Sifat Hossain <sifatb910@gmail.com>
2021-07-25 20:44:00 +0600

    Add solution for problem `A - Subsequence Permutation` Link: https://codeforces.com/contest/1552/problem/A Tags: sortings, strings, *800 Ref: https://codeforces.com/contest/1552/submission/123721028

---
+++ codeforces/1552/A.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<cstring>
+#include<vector>
+#include<set>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main(){
+    
+    TC(t)
+    {
+        string s,s1="";
+        int n;
+        cin >> n;
+        cin >> s;
+        s1+= s;
+        sort(s1.begin(),s1.end());
+        int c = 0;
+        for (int i = 0;i<s.size();i++)
+        {
+            if(s[i]!=s1[i]) c++;
+        }
+        cout << c << endl;
+    }
+
+    return 0;
+}
