461b44b7c4b6cfc71bc86c4661488351d22e9cd8
Sifat Hossain <sifatb910@gmail.com>
2021-07-29 21:53:00 +0600

    Add solution for problem `A - Cherry` Link: https://codeforces.com/contest/1554/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1554/submission/124168239

---
+++ codeforces/1554/A.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<cstring>
+#include<vector>
+#include<set>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int main(){
+    
+    TC(t)
+    {
+        int n;
+        cin >> n;
+
+        ll a[n+2],temp = -1;
+        ll ma = INT_MIN,ans = INT_MIN;
+
+        a[0] = 0;
+        a[n+1] = 1;
+        
+        FL(n) {
+			cin >> a[i];
+		}
+ 
+		for(int i = 1; i < n; i++) {
+			ans= max(ans, (a[i - 1]* a[i]));
+		}
+        cout << ans << endl;
+    }
+
+    return 0;
+}
