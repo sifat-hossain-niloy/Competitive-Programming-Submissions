4b7102b615aeea01351db1e91abfb34038097caa
Sifat Hossain <sifatb910@gmail.com>
2021-11-14 13:36:00 +0600

    Add solution for problem `A - Mathematical Addition` Link: https://codeforces.com/contest/1589/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1589/submission/135380673

---
+++ codeforces/1589/A.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int main(){
+
+    
+    TC(t)
+    {
+        unsigned ll x,y;
+        cin >> x >> y;
+
+        cout << "-" << (x*x) << " " << (y*y) << endl;
+    }
+    return 0;
+}
