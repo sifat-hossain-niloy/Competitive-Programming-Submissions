42bc0dfa5031fe6a54401c9da38ae30bf483c603
Sifat Hossain <sifatb910@gmail.com>
2021-12-01 21:13:00 +0600

    Add solution for problem `B - Absent Remainder` Link: https://codeforces.com/contest/1613/problem/B Tags: greedy, implementation, sortings, *1000 Ref: https://codeforces.com/contest/1613/submission/137662070

---
+++ codeforces/1613/B.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int main(){
+
+    
+    TC(t)
+    {
+        int n;
+        cin >> n;
+        ll a[n],cnt = 0;
+        FL(n) cin >> a[i];
+        sort(a,a+n);
+
+        for (int i = n-1;i>=0;i--)
+        {
+            if(cnt==n/2)
+            {
+                break;
+            }
+            cout << a[i] << " " << a[0]<< endl;
+            cnt++;
+        }
+    }
+    return 0;
+}
