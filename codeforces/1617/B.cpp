914fd2b5649a9037152f2953bd631698792f900d
Sifat Hossain <sifatb910@gmail.com>
2021-12-16 21:17:00 +0600

    Add solution for problem `B - GCD Problem` Link: https://codeforces.com/contest/1617/problem/B Tags: brute force, constructive algorithms, math, number theory, *900 Ref: https://codeforces.com/contest/1617/submission/139538454

---
+++ codeforces/1617/B.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+void solve(int n)
+{
+ 
+    int mid = n / 2;
+
+    for (int i = mid; i >= 1; i--) {
+        if (__gcd(i, n - i) == 1) {
+            cout << i << " " << n - i << " " << 1 << endl;
+            break;
+        }
+    }
+}
+
+
+int main(){
+
+    
+    TC(t)
+    {
+        int n;
+        cin >> n;
+
+        solve(n-1);
+    }
+    return 0;
+}
