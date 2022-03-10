4e7661e4279e528a850d55d09dbb5687a23d9789
Sifat Hossain <sifatb910@gmail.com>
2022-03-11 02:15:00 +0600

    Add solution for problem `A - Playoff` Link: https://codeforces.com/contest/1651/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1651/submission/149190222

---
+++ codeforces/1651/A.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a ;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int main(){
+
+    
+    TC(t)
+    {
+        ll n;
+        cin >> n;
+
+        cout << binpow(2,n) - 1 << endl;
+        
+    }
+    return 0;
+}
