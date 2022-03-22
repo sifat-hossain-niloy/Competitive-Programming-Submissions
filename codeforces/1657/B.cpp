0999bced6822d495130d5c59c8e7e021668044f3
Sifat Hossain <sifatb910@gmail.com>
2022-03-22 21:13:00 +0600

    Add solution for problem `B - XY Sequence` Link: https://codeforces.com/contest/1657/problem/B Tags: greedy, *800 Ref: https://codeforces.com/contest/1657/submission/150489868

---
+++ codeforces/1657/B.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int main(){
+
+    
+    TC(t)
+    {
+        ll n,b,x,y;
+        cin >> n >> b >> x >> y;
+
+        ll temp = b/x,mod = (x-y);
+        ll ans = temp*(temp+1)*x/2;
+        
+        if(temp<n){
+            ll cnt = n-temp,t2 = x*temp;
+            
+            for(int i = 0;i<cnt;i++)
+            {
+                    if(t2+x <=(b)){
+                        t2+=x;
+                        ans+=t2;
+                    }
+                    else{
+                        t2-=y;
+                        ans+=t2;
+                    }
+                
+            
+            }
+        }
+        else{
+            ll mi = min(n,temp);
+            ans = mi*(mi+1)*x/2;
+            
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
