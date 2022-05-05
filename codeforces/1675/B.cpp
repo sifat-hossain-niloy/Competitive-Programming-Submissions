b814528163ca22e5d34a2ff9d8f14b81c973d334
Sifat Hossain <sifatb910@gmail.com>
2022-05-05 22:09:00 +0600

    Add solution for problem `B - Make It Increasing` Link: https://codeforces.com/contest/1675/problem/B Tags: greedy, implementation, *900 Ref: https://codeforces.com/contest/1675/submission/155991048

---
+++ codeforces/1675/B.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int main(){
+    Fastio
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+        ll a[n];
+        FL(n)
+        {
+            cin >> a[i];
+        }
+        ll hobe = a[n-1],ans = 0;
+        for (int i = n-2;i>=0;i--)
+        {
+            if(a[i]<hobe)
+            {
+                hobe = a[i];
+            }
+            else{
+                if(hobe ==0)
+                {
+                    ans = -1;
+                    break;
+                }
+                while(a[i]>=hobe)
+                {
+                
+                    a[i]/=2;
+                    ans++;
+                    
+                }
+                hobe  = a[i];
+        
+            }
+            
+            
+        }
+        
+        
+        cout << ans << endl;
+    }
+    
+    return 0;
+}
