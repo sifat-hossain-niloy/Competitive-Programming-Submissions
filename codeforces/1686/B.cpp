2636f01df68ef211290deebe5be4777cc627e293
Sifat Hossain <sifatb910@gmail.com>
2022-05-25 23:46:00 +0600

    Add solution for problem `B - Odd Subarrays` Link: https://codeforces.com/contest/1686/problem/B Tags: dp, greedy, *800 Ref: https://codeforces.com/contest/1686/submission/158422344

---
+++ codeforces/1686/B.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int main(){
+    Fastio
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+        FL(n)
+        {
+            cin >> a[i];
+        }
+        int ans = 0,mex = -1;
+        for (int i = 0;i<n;i++)
+        {
+            if(mex==-1) mex= a[i];
+            else if (a[i]<mex)
+            {
+                ans ++;
+                mex = -1;
+            }
+            else{
+                mex = max(mex,a[i]);
+            }
+        }
+        cout << ans << endl;
+    }
+    
+    return 0;
+}
