0d0285322f9194d207e37cc54b56fc621aed61cb
Sifat Hossain <sifatb910@gmail.com>
2022-06-25 21:21:00 +0600

    Add solution for problem `A - NIT orz!` Link: https://codeforces.com/contest/1696/problem/A Tags: bitmasks, greedy, *800 Ref: https://codeforces.com/contest/1696/submission/161768378

---
+++ codeforces/1696/A.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int main(){
+    Fastio
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n,z;
+        cin >> n >> z;
+        ll ans = INT64_MIN;
+
+        FL(n)
+        {
+            ll x;
+            cin >> x;
+            ans = max(ans,x|z);
+            
+        }
+        cout << ans << endl;
+
+        
+    }
+    
+    return 0;
+}
