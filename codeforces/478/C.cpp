3f84aff212ed1ecb8a694844da566ff61eea46da
Sifat Hossain <sifatb910@gmail.com>
2022-07-20 14:53:00 +0600

    Add solution for problem `C - Table Decorations` Link: https://codeforces.com/contest/478/problem/C Tags: greedy, *1800 Ref: https://codeforces.com/contest/478/submission/164986282

---
+++ codeforces/478/C.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("in.txt","r",stdin)
+#define write           freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int main(){
+    Fastio
+    
+    int t;
+    // cin >> t;
+    t = 1;
+    while(t--)
+    {
+        vector<ll> v(3);
+        cin >> v[0] >> v[1] >> v[2];
+        sort(v.begin(),v.end());
+        ll ans = 0;
+        
+        ans = min( (v[0]+v[1]+v[2])/3, v[0]+v[1]);
+        cout << ans << endl;
+        
+    }
+    // 1 1 1   1 1 1
+    
+    return 0;
+}
