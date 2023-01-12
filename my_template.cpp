a93ad032b623739af0cd4199f2d5ac18fa7ae5f2
Md. Sifat Hossain <sifatb910@gmail.com>
2023-01-13 02:05:21 +0600

    Create my_template.cpp

---
+++ my_template.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a %MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+struct DSU {
+    vi e; void init(int N) { e = vi(N,-1); }
+    int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); } 
+    bool sameSet(int a, int b) { return get(a) == get(b); }
+    int size(int x) { return -e[get(x)]; }
+    bool unite(int x, int y) { // union by size
+        x = get(x), y = get(y); if (x == y) return 0;
+        if (e[x] > e[y]) swap(x,y);
+        e[x] += e[y]; e[y] = x; return 1;
+    }
+};
+
+
+
+int main(){
+    Fastio
+    
+    TC(t)
+    {
+    
+    }
+    return 0;
+}
