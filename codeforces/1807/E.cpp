4c518491048773292951c755478080203f7d4ae1
Sifat Hossain <sifatb910@gmail.com>
2023-03-19 21:55:00 +0600

    Add solution for problem `E - Interview` Link: https://codeforces.com/contest/1807/problem/E Tags: binary search, implementation, interactive, *1300 Ref: https://codeforces.com/contest/1807/submission/198199258

---
+++ codeforces/1807/E.cpp
@@ -0,0 +1,161 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long
+#define TC(t)           int t;cin >> t;while(t--)
+#define FL(t)           for(int i=0;i<t;i++)
+#define Y               cout << "YES\n"
+#define N               cout << "NO\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+#define Fastio          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define read            freopen("out.txt","w",stdout)
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+struct segtree {
+
+    int size;
+    vector<ll> seg;
+    
+    void init(int n)
+    {
+        size = 1; 
+        while(size<n)size*=2;
+        seg.assign(2*size,0LL);
+    }   
+    void set(int i ,int v,int x,int lx,int rx)
+    {
+        if(rx-lx == 1)
+        {
+            seg[x] = v;
+            return;
+        }
+        int m = (lx+rx)/2;
+        
+        if(i<m)
+        {
+            set(i,v,2*x+1,lx,m);
+        }
+        else{
+            set(i,v,2*x+2,m,rx);
+        }
+        
+        seg[x] = seg[2*x+1]+seg[2*x+2];
+    }
+    
+    void set(int i ,int v)
+    {
+        set(i,v,0,0,size);
+        
+    }
+    void build(vector<ll> &a,int x,int lx,int rx)
+    {
+        if(rx-lx==1)
+        {
+            if(lx<(int)a.size())
+            {
+                seg[x] = a[lx];
+            }
+            return;
+        }
+        int m  =(lx+rx)/2;
+        build(a,2*x+1,lx,m);
+        build(a,2*x+2,m,rx);
+        seg[x] = seg[2*x+1] +seg[2*x+2];
+    }
+    
+    void build (vector<ll> &a)
+    {
+        build(a,0,0,size);
+    }
+    ll query(int l,int r,int x,int lx,int rx)
+    {
+        if(r<=lx || l>=rx)
+        {
+            return 0;
+        }
+        if(lx>=l && rx<=r)
+        {
+            return seg[x];
+        }
+        int m = (lx+rx)/2;
+        
+        ll s1 = query(l,r,2*x+1,lx,m)+query(l,r,2*x+2,m,rx);
+        
+        return s1; 
+    }
+    
+    ll query(int l,int r)
+    {
+        return query(l,r,0,0,size);
+    }
+};
+
+
+
+
+
+int main(){
+    Fastio
+
+    TC(t)
+    {
+        int n,q;
+        cin >> n ;
+        segtree st;
+        st.init(n);
+
+        vector<ll> a(n);
+
+        FL(n) cin >> a[i];
+
+        st.build(a);
+
+        ll l = 1,r = n;
+
+        while(l<r)
+        {
+            int mid = (l+r)/2;
+
+            cout << "? " << (mid-l+1) << " ";
+            for (int i = l;i<=mid;i++)
+            {
+                cout << i << " ";
+            } 
+            cout << endl;
+            ll x;
+            cin >> x;
+
+            ll sum = st.query(l-1,mid);
+            // cout << sum << endl;
+            if(sum==x)
+            {
+                l = mid+1;
+            }
+            else{
+                r = mid;
+            }
+        }
+        cout <<"! " << r << endl;
+
+    }
+    
+   
+    return 0;
+}
